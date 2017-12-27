0703baac35c203d04ec7d9237c125b1d629f07b3
DocenkoG <docn@mail.ru>
2017-12-27 12:32:53 +0300

    В конфиги добавлен период устаревания

--- inter.py
+++ inter.py
@@ -301,9 +301,9 @@ def processing(cfgFName):
         #os.system( dealerName + '_converter_xlsx.xlsm')
         convert2csv( cfgFName)
     folderName = os.path.basename(os.getcwd())
-    #if os.path.exists( csvFName    ) : shutil.copy2( csvFName ,    'c://AV_PROM/prices/' + folderName +'/'+csvFName )
-    #if os.path.exists( 'python.log') : shutil.copy2( 'python.log', 'c://AV_PROM/prices/' + folderName +'/python.log')
-    #if os.path.exists( 'python.1'  ) : shutil.copy2( 'python.log', 'c://AV_PROM/prices/' + folderName +'/python.1'  )
+    if os.path.exists( csvFName    ) : shutil.copy2( csvFName ,    'c://AV_PROM/prices/' + folderName +'/'+csvFName )
+    if os.path.exists( 'python.log') : shutil.copy2( 'python.log', 'c://AV_PROM/prices/' + folderName +'/python.log')
+    if os.path.exists( 'python.1'  ) : shutil.copy2( 'python.log', 'c://AV_PROM/prices/' + folderName +'/python.1'  )
 
 
 
